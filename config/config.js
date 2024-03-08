43c4e759081ea14630ca75f0eac44e016359183b
hernanku <akuhernandez@outlook.com>
2024-03-08 06:34:15 -0500

    :sparkles: intial user model configs

---
+++ config/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_DATABASE,
+    host: process.env.DEV_DB_HOST,
+    dialect: "postgres",
+    logging: false,
+  },
+}
